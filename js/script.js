010204044902cbc9e8cb09d9425f47f6dcc7a186
Draclyr ( Hesam Shahmoradi ) <draclyr@gmail.com>
2024-10-12 12:18:40 -0700

    navbar

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = ' var(--second-bg-color)';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
